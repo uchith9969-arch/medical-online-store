bb0793ac533a6043790fe5d5d5e17272bcb4e686
IT25103962 <IT25103962@my.sliit.lk>
2026-05-17 14:46:37 +0530

    Create Paymentstatus file

--- backend/medical-online-store/src/main/java/com/example/medical_online_store/model/PaymentStatus.java
+++ backend/medical-online-store/src/main/java/com/example/medical_online_store/model/PaymentStatus.java
@@ -1,4 +1,9 @@
 package com.example.medical_online_store.model;
 
 public enum PaymentStatus {
+    UNPAID,
+    PAID,
+    DELIVERED,
+    CANCELLED,
+    REFUNDED
 }
--- backend/medical-online-store/src/main/java/com/example/medical_online_store/model/PaymentType.java
+++ backend/medical-online-store/src/main/java/com/example/medical_online_store/model/PaymentType.java
@@ -1,4 +1,6 @@
 package com.example.medical_online_store.model;
 
 public enum PaymentType {
+    CASH,
+    CARD
 }
